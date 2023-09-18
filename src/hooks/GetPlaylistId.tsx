a8e196ecdd27742181d26ace8a0ab3071be6cd99
Brian Elmo <brianelmo14@gmail.com>
2023-09-18 20:31:16 -0300

    borrando hooks sin usar

--- src/hooks/GetPlaylistId.tsx
+++
@@ -1,9 +0,0 @@
-import { GetPlaylist } from "../services/PLaylistInfo";
-
-
-export function GetPlaylistId(event:React.MouseEvent) { 
-    const clickElement = event.currentTarget
-    const idElement = clickElement.id  
-
-    GetPlaylist(idElement)
-}
