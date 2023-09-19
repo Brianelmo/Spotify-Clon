530d7bdb882fb8fa02092572820bcb9214603e3c
Brian Elmo <brianelmo14@gmail.com>
2023-09-18 21:08:10 -0300

    quitando console.log y cambiando horario dinamico por fijo

--- src/services/PlayListGral.service.tsx
+++ src/services/PlayListGral.service.tsx
@@ -7,24 +7,23 @@ function PlayListGralservice() {
 
   const [gralData, setGralData] = useState<PlaylistData | null>(null); 
 
-    const actualDate = new Date() 
+    // const actualDate = new Date() 
 
-    const dia = actualDate.toISOString().split('T')[0];  
+    // const dia = actualDate.toISOString().split('T')[0];  
 
-    const hora = actualDate.getHours();
+    // const hora = actualDate.getHours();
 
-    let fechaHora = dia + ' ' + hora; 
+    // let fechaHora = dia + ' ' + hora; 
 
 
 
   useEffect(() => {
-    console.log(fechaHora)
     const fetchGralData = async () => {
       const token = localStorage.getItem("token");
 
       try {
         const response = await axios.get<PlaylistData>(
-          `https://api.spotify.com/v1/browse/featured-playlists?timestamp=${dia}T${hora}%3A00%3A00&limit=7`,
+          `https://api.spotify.com/v1/browse/featured-playlists?timestamp=2023-08-18T21:00:00%3A00%3A00&limit=7`,
           {
             headers: {
               Authorization: `Bearer ${token}`,
